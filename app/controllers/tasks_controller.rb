509f9b7a966eb48be1bf0863533726f9ba61eecd
shekhar-patil <patilshekhar900@gmail.com>
2019-01-31 12:34:53 +0530

    add form in new template

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,5 +3,16 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    
+  end
+
+
+  private 
+
+  def task_params
+    params.required(:task).permit(:description)    
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
   get '/index', to: 'tasks#index'
+  get '/new' ,  to: 'tasks#new'
   
 end
