230b4de3dc6555c6aa7edf1c871f8aa34a96998a
parasbansal-87 <parasbansal33@gmail.com>
2019-02-12 10:55:03 +0530

    add validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_29_122730) do
+ActiveRecord::Schema.define(version: 2019_02_11_162942) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "description"
+    t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
