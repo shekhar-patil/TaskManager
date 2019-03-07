e768a4305397d2f92c9748ec5900798733ad2cef
shekhar-patil <patilshekhar900@gmail.com>
2019-03-08 00:59:52 +0530

    Done with chapter no. 13

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
   
-  get   '/tasks',               to: 'tasks#index'
-  get   '/tasks/new' ,          to: 'tasks#new'
-  post  '/tasks',               to: 'tasks#create'
-  get   '/tasks/:id',           to: 'tasks#show', as: 'task'
-  get   '/tasks/:id/edit',      to: 'tasks#edit', as: 'edit_task' 
-  patch '/tasks/:id',           to: 'tasks#update'
-  delete'/tasks/:id',           to: 'tasks#destroy'
-  post '/tasks/update_state',   to: 'tasks#update_state', as: 'update_state'
   root to: 'tasks#index'
   resources :users, only: [:new, :create, :show]
 
   get   '/login'  => 'sessions#new', as: 'new_login'
   post  '/login'  => 'sessions#create'
   delete'/logout' => 'sessions#destroy'
+
+  resources :tasks do
+    resources :status, only: [:update]
+  end
 end
