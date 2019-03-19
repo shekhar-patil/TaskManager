e2b101763d20aae502410326ea92914cdadcc570
parasbansal-87 <parasbansal33@gmail.com>
2019-03-19 18:24:38 +0530

    Add pundit gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,22 @@ class ApplicationController < ActionController::Base
     def require_login
       unless logged_in?
         flash[:danger] = "Please Log In to view the Tasks."
-        redirect_to new_login_url
+        redirect_to new_login_path
+      end
+    end
+
+    def logged_in?
+      !current_user.nil?
+    end
+  
+    def current_user
+      if session[:user_id]
+        @current_user ||= User.find(session[:user_id])
       end
     end
 
     def user_not_authorized
-      flash[:warning] = "You are not authorized to view any task."
-      redirect_to(root_path)
+      flash[:warning] = "All accesssable tasks are listed below."
+      redirect_to root_path
     end
 end
--- app/policies/task_policy.rb
+++ app/policies/task_policy.rb
@@ -1,19 +1,49 @@
 class TaskPolicy
-  attr_reader :user, :tasks
-  # , :tasks_created, :tasks_assigned
+  attr_reader :user, :task
 
-  def initialize(user, tasks)
+  def initialize(user, task)
     raise Pundit::NotAuthorizedError, "must be logged in" unless user
     @user = user
-    @tasks = tasks
+    @task = task
   end
 
-  # CRUD actions
-  def can_access_created_task?
-    tasks.pluck(:creator_id).uniq == [user.id] || tasks.blank?
+  # show? action is invoked when we call `authorize @task` in show action of the controller.
+  # Here the condition that we want to check is whether the record's creator is current user or record is assigned to current user.
+  def show?
+    task.creator_id == user.id || task.assignee_id == user.id
   end
 
-  def can_access_assigned_task?
-    tasks.pluck(:user_id).uniq == [user.id] || tasks.blank?
+  # The condition to make sure a task is editable is the same as that of the show action.
+  # So all we'll do is simply call `show?` inside
+  def edit?
+    show?
+  end
+
+  # Similar is the case for update?
+  def update?
+    show?
+  end
+
+  # Every user can create a task, hence create? will always return true
+  def create?
+    true
+  end
+
+  # Only the user that has created the task and delete it
+  def destroy?
+    task.creator_id == user.id
+  end
+
+  class Scope
+    attr_reader :user, :scope
+    
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope.where(creator_id: user.id).or(scope.where(assignee_id: user.id))
+    end
   end
 end
